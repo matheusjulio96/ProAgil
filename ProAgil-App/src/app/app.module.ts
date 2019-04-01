f7b26591cdb9a0c3ad903e63f6c3f2cc1a4a1801
Matheus Arruda <matheus.arruda@intranet.optcode.com.br>
2019-04-01 15:22:48 -0300

    Angular - Organizando Projeto

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -2,25 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+import { TooltipModule, BsDropdownModule, ModalModule } from 'ngx-bootstrap';
+
+import { EventoService } from './_services/evento.service';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
+
+import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
+
 
 @NgModule({
    declarations: [
       AppComponent,
       EventosComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipePipe
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      TooltipModule.forRoot(),
+      BsDropdownModule.forRoot(),
+      ModalModule.forRoot(),
+    ],
+   providers: [
+     EventoService
    ],
-   providers: [],
    bootstrap: [
       AppComponent
    ]
